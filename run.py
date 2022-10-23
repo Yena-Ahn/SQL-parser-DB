671018e883dcbe5d057dcdf1c90a5f25e84c3740
Yena Ahn <ahnyena02@gmail.com>
2022-10-24 04:45:10 +0900

    fix bug

--- run.py
+++ run.py
@@ -1,123 +1,19 @@
-# from bsddb3 import db
 from berkeleydb import db
-from lark import Lark, Transformer
+from lark import Lark
+from table import *
+from parser import MyTransformer
 
+"""
+Project 1-1 UPDATE
+1. parse information and recognise what query has been received
+2. Syntax error when query is not under grammar rule
 
-class MyTransformer(Transformer):
-    """
-    1. join items of parent nodes of each query - command, query_list and query
-    2. get information of query (parse only the first item of each query for this assignment)
-    """
-    EXIT = str
-    PRIMARY = str
-    KEY = str
-    FOREIGN = str
-    REFERENCES = str
-    IDENTIFIER = str
-    LP = str
-    RP = str
-    TYPE_CHAR = str
-    NOT = str
-    NULL = str
-    CREATE = str
-    TABLE = str
-    TYPE_INT = str
-    TYPE_CHAR = str
-    INT = int
-    
-    def __init__(self):
-        self.table_dict = {}
-    #parent nodes of queries
-    # command = lambda self, items: "".join(map(str,items))
-    # query_list = lambda self, items: "".join(map(str,items))
-    #query = lambda self, items: "".join(map(str, items))
-    def command(self, items):
-        return self.table_dict
-    
-    def query_list(self, items):
-        return items
-    
-    def query(self, items):
-        return items
-    
-    def create_table_query(self, items):
-        self.table_dict["table_name"] = items[2]
-        return items
-    
-    def data_type(self, items):
-        if items[0] == 'char':
-            return [items[0], items[2]]
-        return items
-    
-    def table_name(self, table_name):
-        return table_name
-    
-    def table_element_list(self, table_element):
-        result = [i for i in table_element if i != '(' and i != ')']
-        self.table_dict["table_element"] = result
-        return result
-    
-    def table_element(self, items):
-        return items[0]
-    
-    def table_constraint_definition(self, items):
-        return items[0]
-        
-    def column_definition(self, column_definition):
-        if "column_name" not in self.table_dict.keys():
-            self.table_dict["column_name"] = []
-        if column_definition[-2:] != ['not', 'null']:
-            self.table_dict["column_name"] += [column_definition[:2]]
-            return column_definition[:2]
-        self.table_dict["column_name"] += [column_definition]
-        return column_definition
-    
-    def column_name(self, column_name):
-        return column_name[0]
-    
-    def column_name_list(self, column_name_list):
-        result = [i for i in column_name_list if i != '(' and i !=')']
-        return result
-    
-    def primary_key_constraint(self, primary_key_constraint):
-        primary_key_constraint[0] = primary_key_constraint[0] +  primary_key_constraint[1]
-        if "primary_key" not in self.table_dict.keys():
-            self.table_dict["primary_key"] = []
-        self.table_dict["primary_key"] += primary_key_constraint[2:]
-        return [primary_key_constraint[0]] + primary_key_constraint[2:]
-    
-    def referential_constraint(self, items):
-        items[0] = items[0] + items[1]
-        if "foreign_key" not in self.table_dict.keys():
-            self.table_dict["foreign_key"] = []
-        self.table_dict["foreign_key"] += items[2:]
-        return [items[0]] + items[2:]
-    
-    ###########################################
-    
-    def drop_table_query(self, items):
-        return sum(items, [])
-    
-    def desc_table_query(self, items):
-        return items
-    
-    def show_table_query(self, items):
-        return " ".join(map(str, items))
-    
-    ##########################################
-    def insert_query(self, items):
-        return f"'{items[0].upper()}' requested"
-    
-    def delete_query(self, items):
-        return f"'{items[0].upper()}' requested"
-    
-    def select_query(self, items):
-        return f"'{items[0].upper()}' requested"
-    
-    
-    
-    def update_tables_query(self, items):
-        return f"'{items[0].upper()}' requested"
+Project 1-2 UPDATE
+1. get parsed information with dictionary object or command query
+2. create table object to store data
+3. store in DB using berkeley db
+"""
+
 
 
 with open('grammar.lark') as file:
@@ -127,13 +23,14 @@ def parser(query):
     try:
         result = sql_parser.parse(query)
     except Exception:
-        raise SyntaxError("Syntax error") # rasie SyntaxError when exception occurs e.g. UnexpectedInput
+        raise SyntaxError("Syntax error") # raise SyntaxError when exception occurs e.g. UnexpectedInput
     else:
         transformed = MyTransformer().transform(result)
         if transformed == 'exit':
             global endloop
             endloop = False
         else:
+            #return transformed
             print("DB_2022-81863>",transformed)
         
         
@@ -154,6 +51,33 @@ def query_sequence():
             query_list[i] = query_list[i] + ";"
     return query_list
 
+
+
+
+def database(parsed_dict):
+    if parsed_dict["query"] == "create":
+        #DuplicateColumnDefError
+        if DuplicateColumnDefError(parsed_dict):
+            print("Create table has failed: column definition is duplicated")
+            return False
+        #DuplicatePrimaryKeyDefError
+        if len(parsed_dict["primary_key"]) > 1:
+            print("Create table has failed: primary key definition is duplicated")
+            return False
+        #if 
+        
+                
+def DuplicateColumnDefError(parsed_dict):
+    col_list = [col[0] for col in parsed_dict["column_list"]]
+    no_duplicate = [col[0] for col in parsed_dict["column_list"] if col[0] not in no_duplicate]
+    if len(col_list) != len(no_duplicate):
+        return True
+    return False
+"""
+def ReferenceTypeError(parsed_dict):
+    for i in parsed_dict["foreign_key"]:
+        if len(i[0]) != len(i[2]) or 
+"""
 endloop = True
 
 while endloop:
