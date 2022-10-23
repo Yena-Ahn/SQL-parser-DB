f7d40e4548f3abfe67e462937bbfc426e05ab947
Yena Ahn <ahnyena02@gmail.com>
2022-10-24 04:43:12 +0900

    edit dict object with table class

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
 
 from lark import Lark, Transformer
+from table import *
+
 
 class MyTransformer(Transformer):
     
@@ -29,9 +31,11 @@ class MyTransformer(Transformer):
     TYPE_INT = str
     TYPE_CHAR = str
     INT = int
+    DESC = str
     
     def __init__(self):
         self.table_dict = {}
+        self.table_dict["error"] = []
         
     """
     *PRJ 1-1* parent nodes of queries 
@@ -41,6 +45,8 @@ class MyTransformer(Transformer):
     """
     
     def command(self, items):
+        if items[0] == "exit":
+            return "exit"
         return self.table_dict
     
     def query_list(self, items):
@@ -50,11 +56,15 @@ class MyTransformer(Transformer):
         return items
     
     def create_table_query(self, items):
-        self.table_dict["table_name"] = items[2][0]
+        self.table_dict["query"] = "create"
+        self.table_dict["table_name"] = Table(items[2][0])
+        self.table_dict["table_name"].addColList(self.table_dict["column_list"])
         return items
     
     def data_type(self, items):
         if items[0] == 'char':
+            if items[2] < 1:
+                self.table_dict["error"].append("CharLengthError")
             return [items[0], items[2]]
         return items
     
@@ -72,13 +82,21 @@ class MyTransformer(Transformer):
         return items[0]
         
     def column_definition(self, column_definition):
+        not_null = False
         if "column_list" not in self.table_dict.keys():
             self.table_dict["column_list"] = []
         if column_definition[-2:] != ['not', 'null']:
-            self.table_dict["column_list"].append(column_definition[:2] + [False])
-            return column_definition[:2]
-        self.table_dict["column_list"].append(column_definition[:2] + [True])
+            not_null = True
+        if column_definition[1][0] == "char":
+            col = Column(column_definition[0], column_definition[1][0], column_definition[1][1], not_null)
+        if column_definition[1][0] == "int":
+            col = Column(column_definition[0], column_definition[1][0], None, not_null)
+        self.table_dict["column_list"].append(col)
         return column_definition
+        #col = Column(column_definition[0], column_definition[1][0], column_definition[1][1], True)
+        #self.table_dict["column_list"].append(column_definition[:2] + [True])
+
+        #return column_definition
     
     def column_name(self, column_name):
         return column_name[0]
@@ -89,13 +107,30 @@ class MyTransformer(Transformer):
     
     def primary_key_constraint(self, primary_key_constraint):
         primary_key_constraint[0] = primary_key_constraint[0] +  primary_key_constraint[1]
-        if "primary_key" not in self.table_dict.keys():
-            self.table_dict["primary_key"] = []
-        self.table_dict["primary_key"] += sum(primary_key_constraint[2:], [])
+        if "primary_key" in self.table_dict.keys():
+            #print("Create table has failed: primary key definition is duplicated")
+            self.table_dict["error"] += ["DuplicatePrimaryKeyDefError"]
+        self.table_dict["primary_key"] = []
+        primary_key_list = sum(primary_key_constraint[2:], [])
+        exist = False
+        count = 0
+        
+        for key in primary_key_list:
+            for col in self.table_dict["column_list"]:
+                if key == col.getColName():
+                    col.setPK()
+                    self.table_dict["primary_key"].append(col)
+                    count += 1
+                    exist = True
+        if not exist or count != len(primary_key_list):
+            self.table_dict["error"].append(f"NonExistingColumnDefError({#colName})")
+            
         return [primary_key_constraint[0]] + primary_key_constraint[2:]
     
     def referential_constraint(self, items):
         items[0] = items[0] + items[1]
+        if len(items[2]) != len(items[5]):
+            self.table_dict["error"].append("ReferenceTypeError")
         if "foreign_key" not in self.table_dict.keys():
             self.table_dict["foreign_key"] = []
         self.table_dict["foreign_key"].append([items[2]] + items[4:])
@@ -104,13 +139,18 @@ class MyTransformer(Transformer):
     ###########################################
     
     def drop_table_query(self, items):
-        return sum(items, [])
+        self.table_dict["query"] = "drop"
+        self.table_dict["table_name"] = items[2][0]
+        return items
     
     def desc_table_query(self, items):
+        self.table_dict["query"] = "desc"
+        self.table_dict["table_name"] = items[1][0]
         return items
     
     def show_table_query(self, items):
-        return " ".join(map(str, items))
+        self.table_dict["query"] = "show"
+        return items
     
     ##########################################
     
